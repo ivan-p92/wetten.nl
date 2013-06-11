fbb5d2482a3ad70b7d2a2dab834e2fa92b933555
Ivan Plantevin <ivan.p92@gmail.com>
2013-06-11 13:42:30 +0200

    Sort methods changed to use multiple (three) methods (see description)
    1: In degree
    2: Degree centrality
    3: Betweenness centrality

--- QueryNetwork.py
+++ QueryNetwork.py
@@ -181,7 +181,10 @@ class QueryNetwork:
         """
         predecessors = self.G.predecessors(entity)
         successors = self.G.successors(entity)
-        return list(set(predecessors + successors))
+        
+        neighbours = list(set(predecessors + successors))
+        
+        return neighbours
     
     def getRelatedEntities(self, entity):
         """
@@ -208,31 +211,25 @@ class QueryNetwork:
         return [directNeighbours] + [indirectNeighbours]"""
         return [directNeighbours]
     
-    def sortRelatedEntities(self, entity):
+    def sortRelatedEntities(self, entity, maxResults = 0):
         """
         Retrieves and sorts related entities. Separates entities from the
-        same BWB and those from other BWB's
-        @param entity: entity for which related entities will be taken
+        same BWB and those from other BWB's.
+        
+        @param entity: entity for which related entities will be taken.
+        @param maxResults: maximum number of sorted entities to return.
+            If all entities are required, provide 0 (default).
+        @return: dictionary with the sorted entities returned by self.sortEntities.
         """
         bwb = entity.split('/')[0]
-        relatedEntities = self.getRelatedEntities(entity)
+        relatedEntities = self.getNeighboursForEntity(entity)
         if not relatedEntities:
             return []
         
-        sortedDirect = self.sortEntities(relatedEntities[0])
+        # Sort entities.
+        sortedEntities = self.sortEntities(relatedEntities, maxResults, separate = True, bwb = bwb)
         
-        """
-        # Use this if indirect neighbours must be used
-        if relatedEntities[1]:
-            sortedIndirect = self.sortEntities(relatedEntities[1])
-        else:
-            sortedIndirect = []
-        
-        allEntities = sortedDirect + sortedIndirect
-        """
-        
-        allEntities = sortedDirect
-        return self.separateInternalFromExternal(allEntities, bwb)
+        return sortedEntities
     
     def separateInternalFromExternal(self, entities, bwb):
         """
@@ -255,66 +252,106 @@ class QueryNetwork:
         
         return {'internal': internal, 'external': external}
     
-    def sortEntitiesForBWB(self, bwb):
+    def sortEntitiesForBWB(self, bwb, maxResults = 0):
         """
         Retrieves all entities for the given BWB code and sorts them.
+        
+        @param bwb: BWB code.
+        @param maxResults: maximum number of sorted entities to return.
+            If all entities are required, provide 0 (default).
         """
         nodes = self.getNodesForBWB(bwb)
         if not nodes:
             return []
         
-        return self.sortEntities(nodes)
+        return self.sortEntities(nodes, maxResults)
     
-    def sortEntities(self, entities):
+    def sortEntities(self, entities, maxResults = 0, separate = False, bwb = None):
         """
-        Returns a list of the given entities, sorted by combining
-        degree centrality and betweenness centrality. This combination
-        is done by first sorting the nodes by degree centrality and betweenness centrality
-        separately and then taking the sum of the indices of the nodes as the new ranking.
-        This means that an entity (e.g. "BWBR0011353/artikel/5.3") with the second highest
-        degree centrality and the sixth highest betweenness centrality gets a ranking of 8,
-        which is then less than an entity that is fourth on degree centrality but second
-        on betweenness.
-        """    
+        Returns a dictionary of lists with each list consisting of the
+        given entities, sorted by different methods. The three methods used
+        are in-degree, degree centrality and betweenness centrality. The
+        maximum final number of returned entities for each method is determined
+        by maxResults.
+        
+        @param entities: list of entities to sort.
+        @param maxResults: maximum number of sorted entities to return.
+            If all entities are required, provide 0 (default).
+        @param separate: whether to separate internal from external sources. If desired,
+            provide bwb code.
+        @param bwb: bwb code to separate internal from external sources. Only required
+            if separation is required.
+        @return: dictionary with list of sorted entities for each method, with the
+            method name as key.
+        """
         if not self.didLoadMeasurements:
             self.loadMeasurements()
         
-        # Retrieve sorted lists of tuples for degree and betweenness centrality    
+        # Retrieve sorted lists of tuples for the three methods 
+        inDegree = self.orderNodesByInDegree(entities)
         degreeCentrality = self.orderNodesByDegreeCentrality(entities)
         betweenness = self.orderNodesByBetweenness(entities)
         
-#         closeness = self.orderNodesByCloseness(entities)
-        #print '\nDegree centralities:\n' + str(degreeCentrality)
-        #print '\n\nBetweenness centralities:\n' + str(betweenness)
+        # Only keep number of desired results, but take separation into account
+        if separate:
+            separatedInDegree = self.separateInternalFromExternal(inDegree, bwb)
+            inDegree = {}
+            if maxResults:
+                inDegree['internal'] = separatedInDegree['internal'][0:maxResults]
+                inDegree['external'] = separatedInDegree['external'][0:maxResults]
+            else:
+                inDegree['internal'] = separatedInDegree['internal']
+                inDegree['external'] = separatedInDegree['external']
+                
+            separatedDegreeCentrality = self.separateInternalFromExternal(degreeCentrality, bwb)
+            degreeCentrality = {}
+            if maxResults:
+                degreeCentrality['internal'] = separatedDegreeCentrality['internal'][0:maxResults]
+                degreeCentrality['external'] = separatedDegreeCentrality['external'][0:maxResults]
+            else:
+                degreeCentrality['internal'] = separatedDegreeCentrality['internal']
+                degreeCentrality['external'] = separatedDegreeCentrality['external']
+                
+            separatedBetweenness = self.separateInternalFromExternal(betweenness, bwb)
+            betweenness = {}
+            if maxResults:
+                betweenness['internal'] = separatedBetweenness['internal'][0:maxResults]
+                betweenness['external'] = separatedBetweenness['external'][0:maxResults]
+            else:
+                betweenness['internal'] = separatedBetweenness['internal']
+                betweenness['external'] = separatedBetweenness['external']
+        else:
+            if maxResults:
+                inDegree = inDegree[0:maxResults]
+                degreeCentrality = degreeCentrality[0:maxResults]
+                betweenness = betweenness[0:maxResults]
         
-        degreeIndices = {}
-        betweennessIndices = {}
-        # Associate each entity with its index (ranking) in both degree and
-        # betweenness centrality.
-        for i in range(len(degreeCentrality)):
-            degreeIndices[degreeCentrality[i][0]] = i
-        for i in range(len(betweenness)):
-            betweennessIndices[betweenness[i][0]] = i
+        resultDict = {'inDegree': inDegree,
+                      'degreeCentrality': degreeCentrality,
+                      'betweenness': betweenness,}
         
-        # Create a list of tuples of the form: 
-        # [(entity, degreeCentralityRanking, BetweennessCentralityRanking)]    
-        mergedIndices = [(k, degreeIndices[k], betweennessIndices[k]) for k in degreeIndices.keys()]
+        return resultDict
+    
+    def printSortedResults(self, resultDict):
+        inDegree = resultDict['inDegree']
+        degreeCentrality = resultDict['degreeCentrality']
+        betweenness = resultDict['betweenness']
+        cutOff = 60
         
-        # Sort the list by taking the sum of the rankings
-        sortedEntities = sorted(mergedIndices, key = lambda x: x[1] + x[2])
-        
-        # Conversion to dictionaries solely for debugging purposes (analyzing
-        # ranking and original degree/betweenness values).
-        degreeCentrality = dict(degreeCentrality)
-        betweenness = dict(betweenness)
-#         closeness = dict(closeness)
-        for entity in sortedEntities:
-            print entity[0] + ', \t\t' + str(degreeCentrality[entity[0]]) + ', \t' + str(betweenness[entity[0]])
-#                 + ', \t' + str(closeness[entity[0]])
-        print '\n\n'
-        
-        # Return a list of only the entities
-        return [entity[0] for entity in sortedEntities]
+        if type(inDegree) is dict:
+            print '\nInternal:'
+            for index in range(inDegree['internal'].__len__()):
+                print 'In: {0:60} Out: {1:60} Betw: {2:60}'.format(inDegree['internal'][index][:cutOff], 
+                                                                   degreeCentrality['internal'][index][:cutOff], 
+                                                                   betweenness['internal'][index][:cutOff])
+            print '\nExternal:'
+            for index in range(inDegree['external'].__len__()):
+                print 'In: {0:60} Out: {1:60} Betw: {2:60}'.format(inDegree['external'][index][:cutOff], 
+                                                                   degreeCentrality['external'][index][:cutOff], 
+                                                                   betweenness['external'][index][:cutOff])
+        else:
+            for index in range(inDegree.__len__()):
+                print 'In: {0:60} Out: {1:60} Betw: {2:60}'.format(inDegree[index][:cutOff], degreeCentrality[index][:cutOff], betweenness[index][:cutOff])
     
     def orderNodesByDegree(self, nodes):
         """
@@ -329,38 +366,58 @@ class QueryNetwork:
         degree.sort(key= lambda x: x[1], reverse=True)
         return degree
     
+    def orderNodesByInDegree(self, nodes):
+        """
+        Returns a list of the given nodes, sorted by their indegree.
+        
+        @param nodes: list of nodes.
+        @return: list of ordered nodes.
+        """
+        # Get the in degree values for the given nodes.
+        sortedNodes = [(k, self.inDegree[k]) for k in nodes]
+        
+        # Sort the nodes and only return the entity descriptions
+        sortedNodes.sort(key= lambda x: x[1], reverse=True)
+        sortedNodes = [node[0] for node in sortedNodes]
+        return sortedNodes
+    
     def orderNodesByDegreeCentrality(self, nodes):
         """
         Returns a list of the nodes and their degree centrality values,
         sorted by degree centrality.
+        
         @param nodes: list of nodes.
         @return: list of tuples in the form of [(node, degreeCentrality)]
         """
         # Get the degree centrality values for given nodes
-        result = [(k, self.degreeCentrality[k]) for k in nodes]
+        sortedNodes = [(k, self.degreeCentrality[k]) for k in nodes]
         
-        # Sort them from large to small
-        result.sort(key= lambda x: x[1], reverse=True)
-        return result
+        # Sort them from large to small, return only node descriptions
+        sortedNodes.sort(key= lambda x: x[1], reverse=True)
+        sortedNodes = [node[0] for node in sortedNodes]
+        return sortedNodes
     
     def orderNodesByBetweenness(self, nodes):
         """
         Returns a list of the nodes and their betweenness centrality values,
         sorted by betweenness centrality.
+        
         @param nodes: list of nodes.
         @return: list of tuples in the form of [(node, betweennessCentrality)]
         """
         # Get the betweenness values for given nodes
-        result = [(k, self.betweenness[k]) for k in nodes]
+        sortedNodes = [(k, self.betweenness[k]) for k in nodes]
         
-        # Sort them from large to small
-        result.sort(key= lambda x: x[1], reverse=True)
-        return result
+        # Sort them from large to small and only return node names
+        sortedNodes.sort(key= lambda x: x[1], reverse=True)
+        sortedNodes = [node[0] for node in sortedNodes]
+        return sortedNodes
     
     def orderNodesByCloseness(self, nodes):
         """
         Returns a list of the nodes and their closeness centrality values,
         sorted by closeness centrality.
+        
         @param nodes: list of nodes.
         @return: list of tuples in the form of [(node, closenessCentrality)]
         """
@@ -374,6 +431,7 @@ class QueryNetwork:
     def entityIsInGraph(self, node):
         """
         Returns True if the node is in the graph and otherwise False.
+        
         @param node: entity description for node.
         @return: True or False
         """
