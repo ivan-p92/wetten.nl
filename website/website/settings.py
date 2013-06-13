d64c7c5f01a20d091b33039095d6de5f7af65dbc
Ivan Plantevin <ivan.p92@gmail.com>
2013-06-13 14:48:42 +0200

    Debug False. Changes to user manual.

--- website/website/settings.py
+++ website/website/settings.py
@@ -4,7 +4,7 @@ assert (1,5,1, 'final', 0) == DJANGO_VERSION
 
 # Django settings for website project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -27,7 +27,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "http://justinian.leibnizcenter.org"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
